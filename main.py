1fec550db0d767ee806146cdfdf711b0c9c80c3f
samenoko-112 <kumagai7554@gmail.com>
2024-05-15 15:52:22 +0900

    レンダラードライバを選択できるように。

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ process_running = False
 
 def main(page:Page):
     page.title = "Scrcpy GUI"
-    page.window_height = 600
+    page.window_height = 800
     page.window_width = 500
+    page.padding = 20
 
     def check_audio_and_video(e):
         if no_audio.value == True and no_video.value == True:
-            page.snack_bar = SnackBar(Text("エラー: 音声・映像両方を無効にすることはできません"), bgcolor=colors.RED)
+            page.snack_bar = SnackBar(Text("エラー: 音声・映像両方を無効にすることはできません",color=colors.WHITE), bgcolor=colors.RED)
             page.snack_bar.open = True
             page.update()
             return
@@ -35,7 +36,7 @@ def main(page:Page):
                     device_list.options.append(dropdown.Option(key=device_id,text=device_id))
                     device_list.update()
         except Exception as e:
-            page.snack_bar = SnackBar(Text(f"エラー: {e}"), bgcolor=colors.RED)
+            page.snack_bar = SnackBar(Text(f"エラー: {e}",color=colors.WHITE), bgcolor=colors.RED)
             page.snack_bar.open = True
             page.update()
     
@@ -44,7 +45,7 @@ def main(page:Page):
         global process_running
 
         if device_list.value == None:
-            page.snack_bar = SnackBar(Text("エラー: デバイスが選択されていません"), bgcolor=colors.RED)
+            page.snack_bar = SnackBar(Text("エラー: デバイスが選択されていません",color=colors.WHITE), bgcolor=colors.RED)
             page.snack_bar.open = True
             page.update()
             return
@@ -81,6 +82,8 @@ def main(page:Page):
                 command.extend([f"--audio-buffer={audio_buffer.value}"])
             if max_fps.value:
                 command.extend([f"--max-fps={max_fps.value}"])
+            if render_driver:
+                command.extend([f"--render-driver={render_driver.value}"])
             
             try:
                 run_button.icon = icons.STOP
@@ -97,7 +100,7 @@ def main(page:Page):
                     run_button.update()
             
             except Exception as e:
-                page.snack_bar = SnackBar(Text(f"Error: {e}"), bgcolor=colors.RED)
+                page.snack_bar = SnackBar(Text(f"Error: {e}",color=colors.WHITE), bgcolor=colors.RED)
                 page.snack_bar.open = True
                 page.update()
                 current_process = None
@@ -122,6 +125,7 @@ def main(page:Page):
     audio_buffer = TextField(label="音声バッファー",suffix_text="ms",expand=True,text_align=TextAlign.RIGHT,hint_text="50")
     video_buffer = TextField(label="映像バッファー",suffix_text="ms",expand=True,text_align=TextAlign.RIGHT,hint_text="0")
     max_fps = TextField(label="最大FPS", suffix_text="fps", text_align=TextAlign.RIGHT, hint_text="60")
+    render_driver = Dropdown(label="レンダラードライバ",options=[dropdown.Option(key="direct3d"),dropdown.Option(key="opengl"),dropdown.Option(key="opengles2"),dropdown.Option(key="opengles"),dropdown.Option(key="metal"),dropdown.Option(key="software")])
 
     run_button = FloatingActionButton(icon=icons.PLAY_ARROW, on_click=run_scrcpy)
 
@@ -147,6 +151,7 @@ def main(page:Page):
             audio_buffer,
         ]),
         max_fps,
+        render_driver,
         run_button
     )
 
